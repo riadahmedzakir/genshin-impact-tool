1b8401b7ea8ef1161521ab6720eac128542ee097
riadahmedzakir <riad_zakir@yahoo.com>
2021-06-03 23:22:06 +0600

    Data scrapped for weapon

---
+++ src/app/shared/constants/weapon-ascension.material.constant.ts
@@ -0,0 +1,38 @@
+export const WeaponAscensionMaterial = {
+    Decarabian: {
+        Item_1: 'Tile of Decarabian\'s Tower',
+        Item_2: 'Debris of Decarabian\'s City',
+        Item_3: 'Fragment of Decarabian\'s Epic',
+        Item_4: 'Scattered Piece of Decarabian\'s Dream'
+    },
+    Guyun: {
+        Item_1: 'Luminous Sands from Guyun',
+        Item_2: 'Lustrous Stone from Guyun',
+        Item_3: 'Relic from Guyun',
+        Item_4: 'Divine Body from Guyun'
+    },
+    Boreal: {
+        Item_1: 'Boreal Wolf\'s Milk Tooth',
+        Item_2: 'Boreal Wolf\'s Cracked Tooth',
+        Item_3: 'Boreal Wolf\'s Broken Fang',
+        Item_4: 'Boreal Wolf\'s Nostalgia'
+    },
+    Elixir: {
+        Item_1: 'Mist Veiled Lead Elixir',
+        Item_2: 'Mist Veiled Mercury Elixir',
+        Item_3: 'Mist Veiled Gold Elixir',
+        Item_4: 'Mist Veiled Primo Elixir'
+    },
+    Gladiator: {
+        Item_1: 'Fetters of the Dandelion Gladiator',
+        Item_2: 'Chains of the Dandelion Gladiator',
+        Item_3: 'Shackles of the Dandelion Gladiator',
+        Item_4: 'Dream of the Dandelion Gladiator'
+    },
+    Aerosiderite: {
+        Item_1: 'Grain of Aerosiderite',
+        Item_2: 'Piece of Aerosiderite',
+        Item_3: 'Bit of Aerosiderite',
+        Item_4: 'Chunk of Aerosiderite'
+    }
+};
