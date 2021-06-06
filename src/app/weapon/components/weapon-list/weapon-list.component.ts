ab1ba6cffe2dda2d1743fae0bc93ea7897cf4933
riadahmedzakir <riad_zakir@yahoo.com>
2021-06-06 13:47:57 +0600

    Weapon details

--- src/app/weapon/components/weapon-list/weapon-list.component.ts
+++ src/app/weapon/components/weapon-list/weapon-list.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { filter as _filter } from 'lodash';
+import { first } from 'rxjs/operators';
 
 import { SwordData } from './../../../shared/weapon-basic-data/swords-data.constant';
 import { stars } from './../../../shared/constants/stars.constant';
@@ -10,6 +11,11 @@ import { ClaymoreData } from './../../../shared/weapon-basic-data/claymore-data.
 import { PolearmData } from './../../../shared/weapon-basic-data/polearm-data.constant';
 import { BowData } from './../../../shared/weapon-basic-data/bow-data.constant';
 import { CatalystData } from './../../../shared/weapon-basic-data/catalyst-data.constant';
+import { IWeaponData } from './../../../shared/interfaces/IWeaponData.interface';
+import { WeaponService } from '../../services/weapon.service';
+import { CommonCharacterAscensionMaterial } from './../../../shared/constants/common-character-ascension.material.constant';
+import { WeaponAscensionMaterial } from './../../../shared/constants/weapon-ascension.material.constant';
+import { CommonWeaponAscensionMaterial } from 'src/app/shared/constants/common-weapon-ascension.material.constant';
 
 @Component({
   selector: 'app-weapon-list',
@@ -26,16 +32,41 @@ export class WeaponListComponent implements OnInit {
     polearm: PolearmData
   };
 
-  weaponList;
+  commonCharacterAscensionMaterial = CommonCharacterAscensionMaterial;
+  weaponAscensionMaterial = WeaponAscensionMaterial;
+  commonWeaponAscensionMaterial = CommonWeaponAscensionMaterial;
+
+  displayColumn = ['Level', 'BaseStats', 'SubStats'];
+  displayColumnRefine = ['Level', 'Description'];
+  displayColumnAscension = ['Phase', 'Mora', 'WeaponAscension', 'CommonWeaponAscensionMaterial', 'CommonMaterial'];
+
+  weaponList: IWeaponData;
+  weaponDetailsList = {};
+
+  isLoading = false;
 
   currentWeaponType = this.activatedRoute.snapshot['_routerState'].url.replace('/', '');
 
   constructor(
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private weaponService: WeaponService
   ) { }
 
   filterWeapon(star: number): void {
     this.weaponList = _filter(this.dataList[this.currentWeaponType], { Rarity: star });
+  }
+
+  getDetails(weapon): void {
+    this.isLoading = true;
+    if (!this.weaponDetailsList[weapon.Name]) {
+      this.weaponService.getWeaponDetails(weapon.Name).pipe(first()).subscribe(res => {
+        this.weaponDetailsList[weapon.Name] = res;
+        this.isLoading = false;
+        console.log(this.weaponDetailsList);
+      });
+    } else {
+      this.isLoading = false;
+    }
   }
 
   ngOnInit(): void {
---
+++ src/app/weapon/services/weapon.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeaponService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getWeaponDetails(name): Observable<any> {
+    return this.http.get(`./assets/weapon-data/${name}.json`);
+  }
+}
